9c7dee71a8d5a849eb1397de99e758231b47ad68
devmullets <devmullets@gmail.com>
2020-05-28 18:11:30 -0400

    add teams / teamplayers

--- app/controllers/team_players_controller.rb
+++ app/controllers/team_players_controller.rb
@@ -1,2 +1,21 @@
 class TeamPlayersController < ApplicationController
+    def index
+        team_players = TeamPlayer.all
+        render json: team_players
+    end 
+
+    def create
+        team_player = TeamPlayer.create(team_player_params)
+        render json: team_player
+    end 
+
+    def destroy
+        TeamPlayer.find(params[:id]).destroy
+    end
+
+    private
+
+    def team_player_params
+        params.require(:team_player).permit(:player_id, :team_id)
+    end 
 end
--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,19 @@
 class TeamsController < ApplicationController
+    def index
+        teams = Team.all
+        render json: teams
+    end 
+
+    def create
+        team = Team.create(team_params)
+        render json: team
+    end 
+
+    
+
+    private
+
+    def team_params
+        params.require(:team).permit(:name, :user_id)
+    end 
 end
