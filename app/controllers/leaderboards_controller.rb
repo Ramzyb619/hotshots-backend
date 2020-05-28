7620e79a2ce95f3dd55e2176821a12e5fe44bf5e
Ramzy Badawy <Ramzy.Badawy@gmail.com>
2020-05-27 23:24:23 -0400

    fixes

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -1,2 +1,7 @@
 class LeaderboardsController < ApplicationController
+
+    def index
+        leaderboard = Leaderboard.all
+        render json: leaderboard
+    end 
 end
--- app/controllers/played_games_controller.rb
+++ app/controllers/played_games_controller.rb
@@ -1,2 +1,3 @@
 class PlayedGamesController < ApplicationController
+    
 end
